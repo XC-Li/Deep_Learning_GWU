99880ada44ec0f4f5975987dd8d6b05b0e6d4ad8
george <george_li@gwu.edu>
2018-11-06 04:56:06 +0000

    Q4_4 complete

--- Quiz4/Quiz4_4.py
+++ Quiz4/Quiz4_4.py
@@ -1,8 +1,12 @@
+"""Quiz 4 """
+
 import torch
 import numpy as np
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
+
+torch.manual_seed(42)
 size = 100
 p = np.linspace(-3, 3, size)
 t = np.exp(-np.abs(p)) * np.sin(np.pi * p)
@@ -12,27 +16,29 @@ t = Variable(torch.from_numpy(t)).float().view(size, -1).cuda()
 
 
 R = 1           # Input size
-S = 20            # Number of neurons
+S = 2000           # Number of neurons
 a_size = 1            # Network output size
 
 model = torch.nn.Sequential(
     torch.nn.Linear(R, S),
     torch.nn.ReLU(),
-    torch.nn.Linear(S, a_size),
+    torch.nn.Linear(S, a_size)
 )
 model.cuda()
 
 performance_index = torch.nn.MSELoss()
 
-learning_rate = 1e-1
-max_epoch = 100
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+learning_rate = 0.1
+max_epoch = 5000
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 for epoch in range(max_epoch):
 
     a = model(p)
     loss = performance_index(a, t)
     print(epoch, loss.item())
+    if loss.item() < 1e-4:
+        break
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
@@ -43,7 +49,7 @@ prediction = model(p).detach().cpu().numpy()
 real = t.cpu().numpy()
 x = p.cpu().numpy()
 plt.plot(x, real, label="Actual")
-plt.plot(x, prediction, label="NN Prediction")
+plt.scatter(x, prediction, label="NN Prediction")
 plt.legend()
 plt.title("title")
 plt.show()
