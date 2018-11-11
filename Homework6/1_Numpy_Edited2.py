a793cca2ffe766623dad9ee1f1c24433950e9174
george <george_li@gwu.edu>
2018-11-11 04:35:21 +0000

    E2 complete

--- Homework6/1_Numpy_Edited2.py
+++ Homework6/1_Numpy_Edited2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import timeit
+import matplotlib.pyplot as plt
 
 #----------------------------------------------------------------------------
 Batch_size = 64     # Batch size
@@ -18,31 +19,18 @@ w2 = np.random.randn(S, a)
 learning_rate = 1e-6
 #----------------------------------------------------------------------------
 # Change dtype to Torch Tensor
-p = torch.from_numpy(p)
-t = torch.from_numpy(t)
-w1 = torch.from_numpy(w1)
-w2 = torch.from_numpy(w2)
+p = torch.from_numpy(p).cuda()
+t = torch.from_numpy(t).cuda()
+w1 = torch.from_numpy(w1).cuda()
+w2 = torch.from_numpy(w2).cuda()
 
 #----------------------------------------------------------------------------
 start_time = timeit.default_timer()
-for index in range(1000):
-
-    # h = p.dot(w1)
-    # h_relu = np.maximum(h, 0)
-    # a_net = h_relu.dot(w2)
-    #
-    # loss = np.square(a_net - t).sum()
-    # print(index, loss)
-    #
-    # grad_y_pred = 2.0 * (a_net - t)
-    # grad_w2 = h_relu.T.dot(grad_y_pred)
-    # grad_h_relu = grad_y_pred.dot(w2.T)
-    # grad_h = grad_h_relu.copy()
-    # grad_h[h < 0] = 0
-    # grad_w1 = p.T.dot(grad_h)
-    #
-    # w1 -= learning_rate * grad_w1
-    # w2 -= learning_rate * grad_w2
+loss_list = []
+grad_w1_list = []
+grad_w2_list = []
+max_epoch = 300
+for index in range(max_epoch):
 
     h = p.mm(w1)
     h_relu = h.clamp(min=0)
@@ -50,6 +38,7 @@ for index in range(1000):
 
     loss = (a_net - t).pow(2).sum()
     print(index, loss.item())
+    loss_list.append(loss.item())
 
     grad_y_pred = 2.0 * (a_net - t)
     grad_w2 = h_relu.t().mm(grad_y_pred)
@@ -58,8 +47,29 @@ for index in range(1000):
     grad_h[h < 0] = 0
     grad_w1 = p.t().mm(grad_h)
 
+    grad_w1_list.append(grad_w1.mean().item())
+    grad_w2_list.append(grad_w2.mean().item())
+
     w1 -= learning_rate * grad_w1
     w2 -= learning_rate * grad_w2
 
 elapsed = timeit.default_timer() - start_time
 print("\nTime:", elapsed)
+
+plt.plot(range(max_epoch), loss_list)
+# plt.yscale("log")
+plt.xscale("log")
+plt.title("MSE")
+plt.show()
+
+plt.plot(range(max_epoch), grad_w1_list)
+# plt.yscale("log")
+plt.xscale("log")
+plt.title("Gradient of W1")
+plt.show()
+
+plt.plot(range(max_epoch), grad_w2_list)
+# plt.yscale("log")
+plt.xscale("log")
+plt.title("Gradient of W2")
+plt.show()
--- Homework6/HW6_Report__Xiaochi_Li.md
+++ Homework6/HW6_Report__Xiaochi_Li.md
@@ -21,3 +21,50 @@ about 0.52s. The reason may be pytorch has some more efficient design about calc
 ### E1Q3
 *Q3: Keep the data size same and change the number of epochs for Q2.
 Compare the timing results. Explain your findings.*
+
+|Number of epoch|Time|
+|----|----|
+|500|0.60|
+|1000|1.13|
+|1500|1.67|
+|2000|2.32|
+
+Time increases when the number of epoch increase, the relationship of increase between these two factors is linear.
+Reason: when the running time for each epoch is same, the more epochs we have, the more total running time will be.
+
+### E1Q4
+*Q4: Increase the data size and keep the number of epochs for Q2 (Hints: Big number for epochs).
+Comapre the timing results. Explain your findings.*
+
+When we double the data size (``` batch_size=128 ```), the running time increase to 4.30s. 
+Reason: When the number of epoch is same, increase data size will lead to more computation.
+
+### E1Q5
+*Keep the data size big and keep the number of epochs big. Change the dtype to 
+torch tensor cuda and compare it with numpy.
+Compare the timing results. Explain your findings.*
+
+10000 epoch with cuda: 18.266, without cuda is 10.85.  
+I think the running time on CPU is shorter because the data is not complex, and it takes some time to transfer the data
+to GPU which slow down the overall speed.
+
+### E2Q1
+Q1: Modify  the "1_Numpy.py" file and change the dtype to torch float tensor.
+Save the vale of the performance and  plot the followings:
+
+i. Performance index with respect to epochs.
+![](./MSE.png)
+
+ii. w1 grad 
+![](./GradW1.png)
+
+iii. w2 grad 
+![](./GradW2.png)
+
+iv. Check your results. Explain each of your plots.
+
+The Gradient Descent algorithm converges after 10 epochs.   
+We can observe that the MSE, gradient of W1 and W2 is close to zero after 10 epochs. Which means the gradient descent
+has found the optimum.
+
+
